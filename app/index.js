cd091add8ecba61a4edc5ae399769a507d54d034
Noel Guillen <noelblas274@gmail.com>
2023-09-11 19:17:09 -0400

    added starter icons and styles

--- app/index.js
+++ app/index.js
@@ -1,12 +1,24 @@
-import { View, Text} from 'react-native';
+import { View, Text, ScrollView, SafeAreaView} from 'react-native';
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from "../components";
 //* view is like a fragment or div
-//* Text is like using a Ptag
+//* Text is like using a P tag
 
 const Home = () => {
+    const router = useRouter();
+
+
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView styles={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+           <Stack.Screen 
+           options={{
+            headerStyle: { backgroundColor: COLORS.lightWhite}
+           }}
+           />
+        </SafeAreaView>
     )
 }
 
