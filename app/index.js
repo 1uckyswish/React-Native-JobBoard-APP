9f379338d8a911c4ffba70ac8397b000d2e3c8d4
Noel Guillen <noelblas274@gmail.com>
2023-09-11 19:01:08 -0400

    starter files

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text} from 'react-native';
+//* view is like a fragment or div
+//* Text is like using a Ptag
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
