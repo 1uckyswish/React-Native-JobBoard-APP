9931d57bba208487b0573be3d19a836fbf344c9b
Noel Guillen <noelblas274@gmail.com>
2023-09-11 22:25:53 -0400

    data from api applied through props

--- app/index.js
+++ app/index.js
@@ -12,13 +12,34 @@ const Home = () => {
 
 
     return (
-        <SafeAreaView styles={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-           <Stack.Screen 
+     <SafeAreaView styles={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+        <Stack.Screen 
            options={{
-            headerStyle: { backgroundColor: COLORS.lightWhite}
+            headerStyle: { backgroundColor: COLORS.lightWhite},
+            headerShadowVisible: false,
+            headerLeft: ()=>(
+                <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+            ),
+            headerRight: () => (
+                <ScreenHeaderBtn iconUrl={images.profile} dimension="60%"/>
+            ),
+            headerTitle: ''
            }}
            />
-        </SafeAreaView>
+
+        <ScrollView showsHorizontalScrollIndicator={false}>
+            <View
+            style={{
+                flex: 1,
+                padding: SIZES.medium
+            }}
+            >
+            <Welcome />
+             <Popularjobs />
+             <Nearbyjobs />
+            </View>
+    </ScrollView>
+    </SafeAreaView>
     )
 }
 
