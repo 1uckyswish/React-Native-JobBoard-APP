e954a934f2039d02ec3e0dcdb3e5be397caa942e
Noel Guillen <noelblas274@gmail.com>
2023-09-12 21:42:33 -0400

    completed card UI styles

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -6,7 +6,7 @@ import { checkImageURL } from '../../../../utils'
 
 const PopularJobCard = ({item, selectedJob, handleCardPress }) => {
   return (
-    <TouchableOpacity style={styles.container(selectedJob, item)} onPress={() => handlePress(item)}>
+    <TouchableOpacity style={styles.container(selectedJob, item)} onPress={() => handleCardPress(item)}>
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
          <Image
           source={{
